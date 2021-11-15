67524bea0e75c8c6ad7da66b2226d4d8e7268513
FeierHan <78088429+Vivifeier@users.noreply.github.com>
2021-11-14 22:38:22 -0500

    Update INFO550_Figure2.R

--- Make/INFO550_Figure2.R
+++ Make/INFO550_Figure2.R
@@ -1,18 +1,16 @@
-
-
 library(here)
-library(table1)
 library(corrgram)
 
 
-here::i_am("INFO550_Figure2.R")
+here::i_am("Make/INFO550_Figure2.R")
 
 
 # READ DATA
-data<-read.csv(here("INFO550_HW4_data.csv"),fileEncoding = "UTF-8")
+data<-read.csv(here("Make","INFO550_HW4_data.csv"),fileEncoding = "UTF-8")
 
 #CORRELATION PLOT
-jpeg(here::here('Figure2.jpg'))
+jpeg(here::here('Make/Figures','Figure2.jpg'))
 corrgram(data[,2:4],lower.panel = panel.shade,upper.panel = panel.cor)
 dev.off()
+
 
