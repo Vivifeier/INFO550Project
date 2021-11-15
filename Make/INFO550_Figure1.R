9b0d0913adc2a68cf77dc07bbe5328dd33ea1d4b
FeierHan <78088429+Vivifeier@users.noreply.github.com>
2021-11-14 22:37:39 -0500

    Update INFO550_Figure1.R

--- Make/INFO550_Figure1.R
+++ Make/INFO550_Figure1.R
@@ -2,18 +2,18 @@
 library(here)
 library(table1)
 
-here::i_am("INFO550_Figure1.R")
+here::i_am("Make/INFO550_Figure1.R")
 
 
 # READ DATA
-data<-read.csv(here("INFO550_HW4_data.csv"),fileEncoding = "UTF-8")
+data<-read.csv(here("Make","INFO550_HW4_data.csv"),fileEncoding = "UTF-8")
 
 
-jpeg(here::here('Figure1.jpg'))
+jpeg(here::here('Make/Figures','Figure1.jpg'))
 par(mfrow=c(1,3))
-hist(data1$cig_smoke,main = "cigarette smoking prevalence")
-hist(data1$toba_smoke,main = "tobacco smoking prevalence")
-hist(data1$toba_use,main = "tobacco use prevalence")
+hist(data$cig_smoke,main = "cigarette smoking prevalence")
+hist(data$toba_smoke,main = "tobacco smoking prevalence")
+hist(data$toba_use,main = "tobacco use prevalence")
 
 dev.off()
 
